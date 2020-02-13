4b5782ba455091ad9965f935a55ef046a551470b
Fabrizio Catinella <fabiecatinella@gmail.com>
2020-02-13 19:30:20 +0000

    Chapter 4 Tests Ok

--- rango/views.py
+++ rango/views.py
@@ -8,7 +8,8 @@ def index(request):
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a><br />" )
-    
+    return render(request, 'rango/index.html', context=context_dict)
+
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a><br />")
+    context_dict = {'name': 'Fabrizio Catinella'}
+    return render(request, 'rango/about.html', context=context_dict)
